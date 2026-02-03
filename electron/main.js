72ab9d9a0fc5f616e1b6565a52b660df485e3ae4
Salvador Agr <salvador.1007.aguilar@gmail.com>
2026-02-03 14:09:51 -0600

    feat: restaurar notch 300x20, centrar iconos del dock y habilitar opacidad en separador

--- electron/main.js
+++ electron/main.js
@@ -17,6 +17,7 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
+            webviewTag: true,
             preload: path.join(__dirname, 'preload.js')
         },
         show: false
